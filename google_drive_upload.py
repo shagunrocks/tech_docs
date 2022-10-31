4bdc4729a15480e1473cfa332ad4f11d5cff97aa
Shagun <108828998+shagunrocks@users.noreply.github.com>
2022-10-31 17:13:54 +0530

    Create google_drive_upload.py

---
+++ google_drive_upload.py
@@ -0,0 +1,16 @@
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+# gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+folder = "1nz9YDxY3nh-Xw2TfqbOkoEbxP2brxj3-"
+
+directory = "D:/hello/hi"
+for f in os.listdir(directory):
+    filepath = os.path.join(directory, f)
+    gfile = drive.CreateFile({'parents': [{"id": folder}], 'title': f})
+    gfile.SetContentFile(filepath)
+    gfile.Upload()
